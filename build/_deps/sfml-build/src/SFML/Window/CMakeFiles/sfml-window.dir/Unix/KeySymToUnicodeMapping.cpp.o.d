a94bb6a756e2eabb61cd67f45d9ed2adee57ca61
jarek <jaro1122334455@gmail.com>
2024-09-21 22:47:40 +0200

    usuniecie build

--- build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToUnicodeMapping.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToUnicodeMapping.cpp.o: \
- /home/jarek/cmake-sfml-project/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToUnicodeMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/jarek/cmake-sfml-project/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToUnicodeMapping.hpp \
- /home/jarek/cmake-sfml-project/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h
